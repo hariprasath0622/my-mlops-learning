e412d9ea23d959a123cb0af72dfe0cc084156be9
HARI PRASATH V <116704078+hariprasath0622@users.noreply.github.com>
2025-02-20 11:50:59 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,5 +1,5 @@
 # Import libraries
-
+import mlflow
 import argparse
 import glob
 import os
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
